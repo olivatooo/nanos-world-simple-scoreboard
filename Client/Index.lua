6a40b837664450eea247f6315e9d037a45b55769
DKong <5950882+FelixHtm@users.noreply.github.com>
2020-09-26 17:11:59 +0200

    Initial Commit

---
+++ Client/Index.lua
@@ -0,0 +1,63 @@
+ScoreboardVisibility = false
+UpdateTimer = nil
+Scoreboard = nil
+
+Package:on(
+    "Load",
+    function()
+        Scoreboard = WebUI("Scoreboard", "file:////UI/Scoreboard.html", false)
+        UpdateTimer = Timer:SetTimeout(300, UpdateCheck)
+    end
+)
+
+Package:on(
+    "Unload",
+    function()
+        Scoreboard:Destroy()
+        Timer:ClearTimeout(UpdateTimer)
+    end
+)
+
+Client:on(
+    "KeyDown",
+    function(keyName)
+        if (keyName == "Tab" and ScoreboardVisibility == false) then
+            UpdatePlayerData()
+            Scoreboard:SetVisible(true)
+            ScoreboardVisibility = true
+        end
+    end
+)
+
+Client:on(
+    "KeyUp",
+    function(keyName)
+        if (keyName == "Tab" and ScoreboardVisibility == true) then
+            Scoreboard:SetVisible(false)
+            ScoreboardVisibility = false
+        end
+    end
+)
+
+function UpdateCheck()
+    if (ScoreboardVisibility) then
+        UpdatePlayerData()
+    end
+end
+
+function UpdatePlayerData()
+    local ResultData = {}
+    for i, player in pairs(NanosWorld:GetPlayers()) do
+        table.insert(
+            ResultData,
+            i,
+            {
+                ["Name"] = player:GetName(),
+                ["Ping"] = player:GetPing(),
+                ["IsLocalPlayer"] = player == NanosWorld:GetLocalPlayer()
+            }
+        )
+    end
+    local ResultString = JSON.stringify(ResultData):gsub('"', "~#~")
+    Scoreboard:CallEvent("SetPlayerData", {ResultString})
+end
